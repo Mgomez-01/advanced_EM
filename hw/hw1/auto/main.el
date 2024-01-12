b705abee0c3e36d1ab8590aee925069f8e07a570
Miguel <u1318856@eng.utah.edu>
2024-01-12 08:20:06 -0700

    updating hw1 with notation changes

--- hw/hw1/auto/main.el
+++ hw/hw1/auto/main.el
@@ -5,25 +5,27 @@
                      '(("article" "12pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("datetime2" "calc")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
     "article"
     "art12"
-    "amsmath"
     "graphicx"
     "xcolor"
     "hyperref"
     "wasysym"
     "mathrsfs"
     "datetime2"
-    "boondox-cal")
+    "boondox-cal"
+    "bm"
+    "amsmath"
+    "amssymb")
    (TeX-add-symbols
     "daystilldue"
     "hourstilldue"
@@ -32,6 +34,8 @@
    (LaTeX-add-labels
     "stokes"
     "deldot")
+   (LaTeX-add-environments
+    '("gathered" LaTeX-env-args ["argument"] 0))
    (LaTeX-add-bibliographies
     "references/references"))
  :latex)
