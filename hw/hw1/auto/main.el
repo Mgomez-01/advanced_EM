cb37dc4a97f3f047922a8f82a6e53c81d346e016
Miguel <u1318856@eng.utah.edu>
2024-01-18 18:05:42 -0700

    updating hw files

--- hw/hw1/auto/main.el
+++ hw/hw1/auto/main.el
@@ -5,12 +5,12 @@
                      '(("article" "12pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("datetime2" "calc")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -34,9 +34,10 @@
     "TimeUntilDue")
    (LaTeX-add-labels
     "stokes"
-    "deldot")
-   (LaTeX-add-environments
-    '("gathered" LaTeX-env-args ["argument"] 0))
+    "deldot"
+    "h_current"
+    "h_b_relation"
+    "displacement_curr")
    (LaTeX-add-bibliographies
     "references/references"))
  :latex)
