a4fd3e70ad8a2586b564ae130f751aab1a452259
Miguel <u1318856@eng.utah.edu>
2024-01-25 00:24:10 -0700

    adding in files for hw 2

---
+++ hw/hw2/auto/main.el
@@ -0,0 +1,38 @@
+(TeX-add-style-hook
+ "main"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("article" "12pt")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("datetime2" "calc")))
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art12"
+    "graphicx"
+    "xcolor"
+    "hyperref"
+    "wasysym"
+    "mathrsfs"
+    "datetime2"
+    "boondox-cal"
+    "bm"
+    "amsmath"
+    "amssymb"
+    "cancel")
+   (TeX-add-symbols
+    "daystilldue"
+    "hourstilldue"
+    "minutestilldue"
+    "TimeUntilDue")
+   (LaTeX-add-bibliographies
+    "references/references"))
+ :latex)
+
